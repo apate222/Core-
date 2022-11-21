f681d94d4edd02b3a4b71f7585b370e60c74c66c
apate222 <apate222@uncc.edu>
2022-11-21 15:16:18 -0500

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Avi Patel on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
