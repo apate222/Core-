9b4c1c83f88a65abd8eb388857de46e770563fb6
Avi Patel <89864019+apate222@users.noreply.github.com>
2022-11-29 11:15:29 -0500

    Update ContentView.swift

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -7,18 +7,32 @@
 
 import SwiftUI
 
+@main
+struct CorePlus: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
 struct ContentView: View {
     
     let workouts = workoutData
     
     var body: some View {
+        
+        // user input
+
+        
+        
         //Z stack - where objects are stacked hortizontally
         NavigationView {
             
             VStack(alignment: .leading) {
-                
+                ScrollView(.vertical, showsIndicators: true) {
                 // Main Card View
-                ZStack {
+                /*ZStack {
                     Image("fitness workout")
                         .resizable()
                         .aspectRatio(contentMode: .fill)
@@ -30,12 +44,9 @@ struct ContentView: View {
                             
                             Spacer()
                             
-                            Text("Full Body Workout")
+                            Text("Full Body Exercises")
                                 .font(.largeTitle)
                                 .fontWeight(.semibold)
-                                .foregroundColor(.white)
-                            Text("For Beginners")
-                                .fontWeight(.regular)
                                 .foregroundColor(.white)
                         }
                         .padding()
@@ -49,16 +60,15 @@ struct ContentView: View {
                 .shadow(radius: 8)
                 .padding()
                 
-                
-                Text("Weekly Plan")
+                Text("Body Parts")
                     .font(.largeTitle)
                     .fontWeight(.semibold)
                     .foregroundColor(.black)
                     .padding()
-                
+                */
                 // Stack where the inner elements are arranged/stacked horizontally
-                ScrollView(.horizontal, showsIndicators: true) {
-                    HStack(spacing: 30) {
+                
+                    VStack(spacing: 20) {
                         // itteration for the 5 cards "workout"
                         ForEach(workouts) { workout in
                             
@@ -69,27 +79,24 @@ struct ContentView: View {
                                 VStack {
                                     Image(workout.image)
                                         .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 1500, height: 115)
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: 380, height: 115)
                                     
                                     //vertical stack - where objects are stacked vertically
                                     VStack {
                                         
                                         Spacer()
                                         
-                                        Text(workout.day)
+                                        Text(workout.bodyPart)
                                             .font(.title)
                                             .fontWeight(.semibold)
-                                            .foregroundColor(.black)
-                                        Text(workout.bodyPart)
-                                            .fontWeight(.semibold)
-                                            .foregroundColor(.black)
+                                            .foregroundColor(.white)
                                     }
                                     .padding()
-                                    .frame(width: 200)
+                                    .frame(height: 10)
                                     .background(Color(.clear))
                                 }
-                                .frame(width: 150, height: 180)
+                                .frame(width: 380, height: 180)
                                 .clipped()
                                 .cornerRadius(20)
                             .shadow(radius: 8)
@@ -102,7 +109,11 @@ struct ContentView: View {
                 
                 Spacer()
             }
-            .navigationTitle("Home Workouts")
+            .navigationTitle("My Core+")
+            .font(.largeTitle)
+            .frame(width: 1500)
+            .fontWeight(.bold)
+            
         }
     }
 }
@@ -118,19 +129,19 @@ struct Workout: Identifiable {
     // var is used because it is indicative of some data thtat will be changed over time
     var id = UUID()
     // variable names with their respected data types
-    var day: String
     var bodyPart: String
     var image: String
-    var routine: [String]
+    var home: [String]
+    var machine: [String]
+    var free: [String]
 }
 
 // array of all the workout days
 // let is indicative of some data that will not be changing, let is better to be used when the data is already set to their values, like shown below
 let workoutData = [
-    Workout(day: "Monday", bodyPart: "Leg", image: "leg", routine: ["Warmup", "Leg Press", "Squats", "Leg extension", "Hamstring curls"]),
-    Workout(day: "Tuesday", bodyPart: "Chest", image: "chest", routine: ["Warmup", "DB chest press", "Incline DB press", "DB flys", "Cable flys"]),
-    Workout(day: "Wednesday", bodyPart: "Back", image: "back", routine: ["Warmup", "Pull ups", "Row machine", "Deadlifts", "Lat pull down"]),
-    Workout(day: "Thursday", bodyPart: "Shoulder", image: "shoulder", routine: ["Warmup", "DB shoulder press", "Shoulder machine", "Cable lateral raises", "Cable horizonal raise"]),
-    Workout(day: "Friday", bodyPart: "Leg", image: "leg", routine: ["Warmup", "Leg Press", "Squats", "Leg extension", "Hamstring curls"])
+    Workout(bodyPart: "Legs", image: "leg", home: ["Squat", "Forward Lunge", "Backward Lunge", "Wall Sit", "Speed Skaters"], machine: ["Leg Press", "Leg Extension", "Leg Curl", "Calf Raise", "Hip Abductor"], free: ["Weighted Squat", "Bulgarian Squat", "Hip Raise", "Romanian Deadlift", "Weighted Lunge"]),
+    Workout(bodyPart: "Chest", image: "chest", home: ["Pushup", "Incline Pushup", "Decline Pushup", "Diamond Pushup", "Wide Pushup"], machine: ["Chest Press", "Chest Flies", "Vertical Cable Flies", "Horizontal Cable Flies", "Seated Cable Press"], free: ["Bench Press", "Dumbell Chest Press", "Incline Bench Press", "Dumbell Flies", "Plate Press"]),
+    Workout(bodyPart: "Back", image: "back", home: ["Pullups", "Chinups", "Weighted Pullup", "Superman", "Back Extension"], machine: ["Lat Pulldown", "Seated Row", "Rear Delt Flies", "Back Extension", "T Bar Row"], free: ["Bent Over Row", "Incline Row", "Upright Row", "Good Morning", "Rack Pull"]),
+    Workout(bodyPart: "Shoulder", image: "shoulder", home: ["Pipe Pushups", "Shoulder Taps", "Arm Circles"], machine: ["Face Pull", "Reverse Fly", "Machine Shoulder Press"], free: ["Arnold Press", "Front Raise", "Lateral Raise", "Overhead Press", "Shrug"]),
+    Workout(bodyPart: "Arms", image: "bicep curls", home: ["Tricep Dip", "Tricep Pushup", "Hand Walk", "Crabwalk", "Inchworm"], machine: ["Preacher Curl", "Tricep Extension", "Cable Kickback"], free: ["Bicep Curl", "Hammer Curl", "Skullcrusher", "Concentration Curl", "Incline Curl"])
 ]
-
